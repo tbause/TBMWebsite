bfc02172e2b9ec7738a03af1d11fdb65eb033ccc
Thomas Bause Mason <thomas.bausemason@outlook.com>
2017-01-20 23:27:55 -0500

    Back to simple

--- src/classes/car.js
+++
@@ -1,9 +0,0 @@
-import {Vehicle} from "./vehicle.js";
-
-export class Car extends Vehicle {
-    constructor(license, model, latLong) {
-        super(license, model, latLong);
-        this.miles = null;
-        this.make  = null;
-    }
-}
